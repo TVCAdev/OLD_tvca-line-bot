ed5450e09355c305aeaffaa6d3c7d5641dcec196
TVCAdev <TVCAdev@gmail.com>
2021-12-31 15:37:47 +0900

    comment out debug code.

--- heroku_app.js
+++ heroku_app.js
@@ -4,12 +4,14 @@ const express = require("express");
 const line = require("@line/bot-sdk");
 
 const app = express();
-//const server = require("http").Server(app);
-const fs = require('fs');
-const server = require('https').createServer({
-    key: fs.readFileSync('./privatekey.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-}, app)
+const server = require("http").Server(app);
+
+// for debug code
+// const fs = require('fs');
+// const server = require('https').createServer({
+//     key: fs.readFileSync('./privatekey.pem'),
+//     cert: fs.readFileSync('./cert.pem'),
+// }, app)
 
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
