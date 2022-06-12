c24aba136793ac726bab9ad7451aba01ab5337ee
TVCAdev <TVCAdev@gmail.com>
2022-06-12 10:29:03 +0900

    add socketio connection less case.

--- heroku_app.js
+++ heroku_app.js
@@ -433,72 +433,73 @@ app.post("/callback", line.middleware(config), (req, res) => {
  */
 function handleEvent(event) {
 
-    // If websocket's connection is none, return error message
-    if (io.engine.clientsCount == 0) {
+    // type is message
+    if (event.type == "message") {
+        // return button template
         return client.replyMessage(event.replyToken, {
-            type: "text",
-            text: "Websocketが接続されていません。",
+            type: "template",
+            altText: "This is a buttons template",
+            template: {
+                type: "buttons",
+                title: "お願いしたいこと",
+                text: "アクションを選択してください。",
+                actions: [
+                    {
+                        "type": "postback",
+                        "label": "リビングの現在画像",
+                        "data": "action=getpic"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "パパの現在地",
+                        "data": "action=getloc"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "TVの禁止設定",
+                        "data": "action=TVStatus"
+                    },]
+            }
         });
-    } else {
-        // type is message
-        if (event.type == "message") {
-            // return button template
-            return client.replyMessage(event.replyToken, {
-                type: "template",
-                altText: "This is a buttons template",
-                template: {
-                    type: "buttons",
-                    title: "お願いしたいこと",
-                    text: "アクションを選択してください。",
-                    actions: [
-                        {
-                            "type": "postback",
-                            "label": "リビングの現在画像",
-                            "data": "action=getpic"
-                        },
-                        {
-                            "type": "postback",
-                            "label": "パパの現在地",
-                            "data": "action=getloc"
-                        },
-                        {
-                            "type": "postback",
-                            "label": "TVの禁止設定",
-                            "data": "action=TVStatus"
-                        },]
+    }
+    // type is postback
+    else if (event.type == "postback") {
+
+        // function for setting sender IDs
+        function set_senderIDs(setIDs) {
+            // get sender ID
+            if (event.source.type == "user") {
+                console.log("user " + event.source.userId);
+                // if userID is not included in setIDs, userID is added.
+                if (!setIDs.includes(event.source.userId)) {
+                    setIDs.push(event.source.userId + "");
                 }
-            });
-        }
-        // type is postback
-        else if (event.type == "postback") {
 
-            // function for setting sender IDs
-            function set_senderIDs(setIDs) {
-                // get sender ID
-                if (event.source.type == "user") {
-                    console.log("user " + event.source.userId);
-                    // if userID is not included in setIDs, userID is added.
-                    if (!setIDs.includes(event.source.userId)) {
-                        setIDs.push(event.source.userId + "");
-                    }
-
-                } else if (event.source.type == "group") {
-                    console.log("group " + event.source.groupId + " " + event.source.userId);
-                    // if groupId is not included in setIDs, groupId is added.
-                    if (!setIDs.includes(event.source.groupId)) {
-                        setIDs.push(event.source.groupId + "");
-                    }
-                } else if (event.source.type == "room") {
-                    console.log("room " + event.source.roomId + " " + event.source.userId);
-                    // if roomId is not included in setIDs, roomId is added.
-                    if (!setIDs.includes(event.source.roomId)) {
-                        setIDs.push(event.source.roomId + "");
-                    }
+            } else if (event.source.type == "group") {
+                console.log("group " + event.source.groupId + " " + event.source.userId);
+                // if groupId is not included in setIDs, groupId is added.
+                if (!setIDs.includes(event.source.groupId)) {
+                    setIDs.push(event.source.groupId + "");
+                }
+            } else if (event.source.type == "room") {
+                console.log("room " + event.source.roomId + " " + event.source.userId);
+                // if roomId is not included in setIDs, roomId is added.
+                if (!setIDs.includes(event.source.roomId)) {
+                    setIDs.push(event.source.roomId + "");
                 }
             }
+        }
 
-            // selected GET LIVING PICTURE
-            if (event.postback.data == "action=getpic") {
+        // selected GET LIVING PICTURE
+        if (event.postback.data == "action=getpic") {
+            // if connection to TVCARouter was lost, reply error message.
+            if (clientConnections == 0) {
+                return client.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "TVCA Routerとの接続が切れているため、実行できません。",
+                });
+            }
+            else {
                 set_senderIDs(getpicIDs)
                 // send GET_LIVINGPIC message to socket.io clients
                 console.log("GET_LIVINGPIC was fired.");
@@ -506,87 +507,87 @@ function handleEvent(event) {
                 // send GET_LIVINGPIC message to socket.io clients(target is raspberry pi.)
                 io.sockets.emit("GET_LIVINGPIC");
             }
-            // selected GET LOCATION
-            else if (event.postback.data == "action=getloc") {
-                set_senderIDs(getlocIDs)
-                console.log("GET_LOCATION was fired.");
+        }
+        // selected GET LOCATION
+        else if (event.postback.data == "action=getloc") {
+            set_senderIDs(getlocIDs)
+            console.log("GET_LOCATION was fired.");
 
-                // send firebase notification to clients(target is father's smartphone.)
-                sendNotification();
-            }
-            // selected BAN TV
-            else if (event.postback.data == "action=TVStatus") {
-                set_senderIDs(getTVStsIDs)
-                console.log("BAN_TV was fired.");
+            // send firebase notification to clients(target is father's smartphone.)
+            sendNotification();
+        }
+        // selected BAN TV
+        else if (event.postback.data == "action=TVStatus") {
+            set_senderIDs(getTVStsIDs)
+            console.log("BAN_TV was fired.");
 
-                // send GET_TV_STATUS message to socket.io clients(target is raspberry pi.)
-                io.sockets.emit("GET_TV_STATUS");
-            }
-            // selected set TVban
-            else if (event.postback.data.startsWith("action=banTVs")) {
-                console.log("set TVbans was fired.");
+            // send GET_TV_STATUS message to socket.io clients(target is raspberry pi.)
+            io.sockets.emit("GET_TV_STATUS");
+        }
+        // selected set TVban
+        else if (event.postback.data.startsWith("action=banTVs")) {
+            console.log("set TVbans was fired.");
 
-                // get changeTo and cec_name from postback data
-                let changeTo = event.postback.data.substr(23, 1);
-                let cec_name = event.postback.data.substr(30);
+            // get changeTo and cec_name from postback data
+            let changeTo = event.postback.data.substr(23, 1);
+            let cec_name = event.postback.data.substr(30);
 
-                // update setting of TV bans.
-                const TVbansRef = db.collection('config').doc('TVbans');
+            // update setting of TV bans.
+            const TVbansRef = db.collection('config').doc('TVbans');
 
-                let set_obj = {};
-                set_obj[cec_name] = changeTo;
-                TVbansRef.update(set_obj)
-                    .then(doc => {
-                        console.log('updating ' + cec_name + ' to ' + changeTo + ' was succeed.');
+            let set_obj = {};
+            set_obj[cec_name] = changeTo;
+            TVbansRef.update(set_obj)
+                .then(doc => {
+                    console.log('updating ' + cec_name + ' to ' + changeTo + ' was succeed.');
 
-                        // send UPDATE_TVBAN message with name to socket.io clients(target is raspberry pi.)
-                        io.sockets.emit("UPDATE_TVBAN", { 'name': cec_name });
-                    })
-                    .catch((error) => {
-                        console.log('updating ' + cec_name + ' to ' + changeTo + ' was failed.: %s', error);
-                    });
-            }
-            // selected show TVban Logs
-            else if (event.postback.data.startsWith("action=showBanTVLogs")) {
-                console.log("show TVban Logs was fired.");
+                    // send UPDATE_TVBAN message with name to socket.io clients(target is raspberry pi.)
+                    io.sockets.emit("UPDATE_TVBAN", { 'name': cec_name });
+                })
+                .catch((error) => {
+                    console.log('updating ' + cec_name + ' to ' + changeTo + ' was failed.: %s', error);
+                });
+        }
+        // selected show TVban Logs
+        else if (event.postback.data.startsWith("action=showBanTVLogs")) {
+            console.log("show TVban Logs was fired.");
 
-                // get changeTo and cec_name from postback data
-                let cec_name = event.postback.data.substr(26);
-                let logtext = cec_name + 'のログ一覧(最新20件)\n';
+            // get changeTo and cec_name from postback data
+            let cec_name = event.postback.data.substr(26);
+            let logtext = cec_name + 'のログ一覧(最新20件)\n';
 
-                // view logs of target cec_name
-                db.collection('log').doc('TVStatus').collection('Logs')
-                    .where('name', '=', cec_name)
-                    .orderBy('date', 'desc').limit(20)
-                    .get()
-                    .then(querySnapshot => {
-                        querySnapshot.forEach(queryDocumentSnapshot => {
-                            let data = queryDocumentSnapshot.data();
-                            let date = data.date.toDate();
-                            let year = date.getFullYear();
-                            let month = ("0" + (date.getMonth() + 1)).slice(-2);
-                            let day = ("0" + date.getDate()).slice(-2);
-                            let hour = ("0" + date.getHours()).slice(-2);
-                            let min = ("0" + date.getMinutes()).slice(-2);
-                            let sec = ("0" + date.getSeconds()).slice(-2);
+            // view logs of target cec_name
+            db.collection('log').doc('TVStatus').collection('Logs')
+                .where('name', '=', cec_name)
+                .orderBy('date', 'desc').limit(20)
+                .get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(queryDocumentSnapshot => {
+                        let data = queryDocumentSnapshot.data();
+                        let date = data.date.toDate();
+                        let year = date.getFullYear();
+                        let month = ("0" + (date.getMonth() + 1)).slice(-2);
+                        let day = ("0" + date.getDate()).slice(-2);
+                        let hour = ("0" + date.getHours()).slice(-2);
+                        let min = ("0" + date.getMinutes()).slice(-2);
+                        let sec = ("0" + date.getSeconds()).slice(-2);
 
-                            let showDate = `${year}/${month}/${day} ${hour}:${min}:${sec}`
-                            logtext = logtext + ' ' + showDate + '-> ' + data.status + 'になりました。\n'
-                        });
-
-                        // send log data
-                        client.replyMessage(event.replyToken, {
-                            type: "text",
-                            text: logtext,
-                        });
+                        let showDate = `${year}/${month}/${day} ${hour}:${min}:${sec}`
+                        logtext = logtext + ' ' + showDate + '-> ' + data.status + 'になりました。\n'
                     });
 
-            }
+                    // send log data
+                    client.replyMessage(event.replyToken, {
+                        type: "text",
+                        text: logtext,
+                    });
+                });
+
         }
-        else {
-            // receive only text message or postback
-            return Promise.resolve(null);
-        }
+    }
+    else {
+        // receive only text message or postback
+        return Promise.resolve(null);
     }
 }
 
